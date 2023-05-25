8d85bd410f58604e24c51e013988beec961fb695
Connor Ferster <38441489+connorferster@users.noreply.github.com>
2023-05-25 02:40:45 -0700

    Initial commit

--- src/loadfactors/__init__.py
+++ src/loadfactors/__init__.py
@@ -5,14 +5,15 @@ of load factors according to your local building code.
 
 __version__ = "0.0.1"
 
-from loadfactors.loadfactors import (
-    Load,
-    open_load_combinations,
-    factored_max,
-    factored_min,
-    factored_max_trace,
-    factored_min_trace,
-    get_factored_matrix,
-    factor_load,
-    alias_to_service_loads
-)
+from loadfactors import loadfactors
+# from loadfactors.loadfactors import (
+#     Load,
+#     open_load_combinations,
+#     factored_max,
+#     factored_min,
+#     factored_max_trace,
+#     factored_min_trace,
+#     get_factored_matrix,
+#     factor_load,
+#     alias_to_service_loads
+# )
